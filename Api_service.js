8308ca2a1837436437ad23affbf59a91b4503db9
Aman Kumar <akamaneng@gmail.com>
2025-11-19 15:14:13 +0530

    ApiDeploy

---
+++ Api_service.js
@@ -0,0 +1,23 @@
+const exp = require("express");
+const app = exp();
+
+app.use(exp.json())
+
+let arr=[
+    {
+        "name":"Aman Kumar",
+        "age":23
+    },
+    {
+       "name":"cash kumari",
+        "age":30 
+    }
+]
+
+app.get("/userData",(userRequest,userResponse)=>{
+    userResponse.status(200).json(arr);
+})
+
+app.listen(3000,()=>{
+    console.log("Server Is Running Successfully")
+})
