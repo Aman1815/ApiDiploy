61c056c3fcc621c916200c1d0b5738d5324be8df
Aman Kumar <akamaneng@gmail.com>
2025-11-21 14:19:11 +0530

    Add put and Deletd method

--- Api_service.js
+++ Api_service.js
@@ -5,10 +5,12 @@ app.use(exp.json())
 
 let arr=[
     {
+        "id":1,
         "name":"Aman Kumar",
         "age":23
     },
     {
+        "id":2,
        "name":"cash kumari",
         "age":30 
     }
@@ -23,6 +25,38 @@ app.post("/userNewData",(newRequest,newresponse)=>{
     const newAdd = newRequest.body;
     arr.push(newAdd);
     newresponse.json(arr);
+})
+
+
+
+app.put("/update/:id", (updateRequest, updateResponse) => {
+    const id = updateRequest.params.id;
+    const { name, age } = updateRequest.body;
+    const indexid = arr.findIndex(i => i.id == id);
+    if (indexid === -1) {
+        console.log("user not found");
+    }
+    else {
+        arr[indexid].name = name;
+        arr[indexid].age = age;
+        updateResponse.status(200).json({
+            message: "user upadte Successfuly"
+        })
+    }
+})
+
+app.delete("/deletd/:id",(deletedRequest,deletedResponse)=>{
+    const {id} = deletedRequest.params;
+    const Indexid = arr.findIndex(i => i.id==id);
+    if(Indexid == -1){
+        return deletedResponse.json({
+            Message:"Not Deleted"
+        })
+    }
+    arr.splice(Indexid,1);
+    return deletedResponse.json({
+        Message:"Deleted Successful"
+    })
 })
 
 
